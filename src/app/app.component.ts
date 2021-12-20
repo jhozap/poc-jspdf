f4b2040e0c215437ff219346b5bee4104f03925c
Jhonny <jhonny.z1022@gmail.com>
2021-12-20 11:11:55 -0500

    implementacion jsPDF con contraseña

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { jsPDF } from "jspdf";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'poc-jspdf';
+
+
+  generatePDF = () => {
+
+    console.log('Print');
+
+    // al instanciar un nuevo documento con jsPDF 
+    // se añade la configuracion de cifrado del documento
+    const doc = new jsPDF({
+      encryption: {
+        userPassword: "user",
+        ownerPassword: "owner",
+        userPermissions: ["print", "modify", "copy", "annot-forms"]
+      }
+    });
+
+    doc.text("Hello world!", 10, 10);
+    doc.save("a4.pdf");
+    doc.autoPrint();
+  }
+
 }
